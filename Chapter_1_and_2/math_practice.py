490fe759e9084dd326ff648177ff8679f44d20e2
rnmcdowell95 <rnmcdowell95@gmail.com>
2021-06-28 17:02:41 -0700

    Organized

---
+++ Chapter_1_and_2/math_practice.py
@@ -0,0 +1,8 @@
+print(4+4)
+print(64/8)
+print(13-5)
+print(2*4)
+
+favorite_number = 7
+
+print(f"This is my favorite number: {favorite_number}")
---
+++ Chapter_3/list.py
@@ -0,0 +1,7 @@
+bicycles = ['trek', 'mountain', 'large', 'small']
+print(bicycles)
+print(bicycles[0])
+print(bicycles[0].title())
+print(bicycles[-1])
+
+print(f"My first bicylce was a {bicycles[0].title()}")
