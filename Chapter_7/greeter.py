ced7f7917fd2bf6c1138bae43e8a6654f140b04a
rnmcdowell95 <rnmcdowell95@gmail.com>
2021-07-14 18:41:19 -0700

    Almsot done with Chapter 7

---
+++ Chapter_7/greeter.py
@@ -0,0 +1,12 @@
+#Ryan McDowell
+#Practicing with user input
+#7/14/21
+
+name = input("Please enter your name: ")
+print(f"\nHello, {name}!")
+
+prompt = "If you tlll us who you are, we can personalize the mesages you see."
+prompt += "\nWhat is your first name? "
+
+name = input (prompt)
+print(f"\nHello, {name}!")
---
+++ Chapter_7/rollercoaster.py
@@ -0,0 +1,11 @@
+#Ryan McDowell
+#Practicing input conversion
+#7/14/21
+
+height = input("How tall are you, in inches? ")
+height = int(height)
+
+if height >= 48:
+    print("\nYou are tall enough to ride!")
+else:
+    print("\nYou'll be able to ride when you grow!")
