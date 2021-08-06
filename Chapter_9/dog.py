6c7c43b818609515f1077416281b802e5d210dce
rnmcdowell95 <rnmcdowell95@gmail.com>
2021-08-05 20:30:56 -0700

    Starting Chapter 9

---
+++ Chapter_9/dog.py
@@ -0,0 +1,21 @@
+#Ryan McDowell
+#8/5/21
+#Creating a class to represent dogs
+
+class Dog:
+    """A simple attempt to model a dog."""
+
+    def __init__(self, name, age):
+        """Initialize name and age attributes."""
+        self.name = name
+        self.age = age
+
+        def sit(self):
+            """Simulate a dog sitting in response to a command."""
+            print(f"{self.name} is now sitting.")
+        
+        def roll_over(self):
+            """"Simulate a dog rolling over in response to a command."""
+            print(f"{self.name} rolled over!")
+
+
