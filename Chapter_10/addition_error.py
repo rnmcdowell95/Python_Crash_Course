7517200f5d12cac330bc749238c2b43c6f618540
rnmcdowell95 <rnmcdowell95@gmail.com>
2021-08-24 14:20:04 -0700

    Chapter 10 Practice

---
+++ Chapter_10/addition_error.py
@@ -0,0 +1,12 @@
+#Ryan McDowell
+#8/24/21
+#Practicing error handling with numbers
+
+print("Hello! Please provide two numbers that you would like to add together.")
+
+first_number = input("Please provide the first number: ")
+second_number = input("Please provide the second number: ")
+
+
+try:
+    
