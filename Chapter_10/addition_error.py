4b68e60d3d0488eb6517823d11a0f0e465ca2d05
rnmcdowell95 <rnmcdowell95@gmail.com>
2021-09-15 17:49:52 -0700

    Chapter 10 practie

--- Chapter_10/addition_error.py
+++ Chapter_10/addition_error.py
@@ -2,6 +2,9 @@
 #8/24/21
 #Practicing error handling with numbers
 
+from typing import cast
+
+
 print("Hello! Please provide two numbers that you would like to add together.")
 
 first_number = input("Please provide the first number: ")
@@ -9,4 +12,13 @@ second_number = input("Please provide the second number: ")
 
 
 try:
+    first_number = int(first_number)
+    second_number = int(second_number)
+    divsion = first_number + second_number
     
+except ZeroDivisionError:
+    print("You can't divide by 0!")
+except ValueError:
+    print("Please enter an integer value.")
+else:
+    print(f"{first_number}+{second_number} = {divsion}")
