9d9161996b93aa08d46850a001e52aace50f0779
rnmcdowell95 <rnmcdowell95@gmail.com>
2021-09-29 16:22:09 -0700

    Chapter 10 Finish

--- Chapter_10/remember_me.py
+++ Chapter_10/remember_me.py
@@ -4,9 +4,43 @@
 
 import json
 
-username = input("Enter your username: ")
+#Load the username, if it has been stored previously
+#Otherwise, prompt for the username and store it.
 
-filename = 'username.json'
-with open(filename, 'w') as f:
-    json.dump(username, f)
-    print(f"Thanks for submitting your name, we'll be ready for you when come back {username}!")
+def get_stored_username():
+    """Get stored username if available."""
+    filename = 'username.json'
+
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def get_new_username():
+    """Get a new username."""
+    username = input("Enter your username: ")
+    filename = 'username.json'
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+
+    return username
+
+
+def greet_user():
+    """Greet the user by name."""
+    
+    username = get_stored_username()
+    
+    
+
+    if username:
+        print(f"Welcome back, {username}!")
+    else:
+        username = get_new_username()
+        print(f"Thanks for submitting your name, we'll be ready for you when come back {username}!")
+
+        
+greet_user()
