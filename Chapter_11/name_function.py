56750e5b83152cc98d466ab7b8dccecc58b8da1d
rnmcdowell95 <rnmcdowell95@gmail.com>
2021-09-30 09:40:33 -0700

    Chapter 11 practice

---
+++ Chapter_11/name_function.py
@@ -0,0 +1,9 @@
+#Ryan McDowell
+#9/29/2021
+#Practicing testing code
+
+
+def get_formatted_name(first, last):
+    """Generates a neatly formatted name."""
+    full_name = f"{first} {last}"
+    return full_name.title()
