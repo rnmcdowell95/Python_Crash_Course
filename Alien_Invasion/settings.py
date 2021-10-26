3b6631d573217d8412230baee3088fdc9caa12c5
rnmcdowell95 <rnmcdowell95@gmail.com>
2021-10-26 12:21:48 -0700

    Alien invasion work

---
+++ Alien_Invasion/settings.py
@@ -0,0 +1,14 @@
+#Ryan McDowell
+#Created 10/26/2021
+#Last updated 10/26/2021
+#Contains the settings for the Alien Invasion project.
+
+class Settings:
+    """A class to store all of the settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initializing the game settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
---
+++ Alien_Invasion/ship.py
@@ -0,0 +1,24 @@
+#Ryan McDowell
+#10/26/2021
+#A class to hold the ship for the Alien Invasion Project.
+
+import pygame
+
+class Ship:
+    """A class to manage the ship."""
+
+    def __init__(self, ai_game):
+        """Initializes the ship and starting position."""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        #Load the ship image and get its rect.
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        #Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """Draw the ship at tis current location."""
+        self.screen.blit(self.image, self.rect)
